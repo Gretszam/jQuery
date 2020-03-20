91a7e4b9d2ca623c0fe4c2eb2bde62bf8bd27551
Gretszam <penguino11111@gmail.com>
2020-03-20 10:14:37 -0700

    new jquerrythings

--- function.js
+++ function.js
@@ -1,27 +1,23 @@
-
+// okay so what i want to do is for the user to be able to select how many "levels" they want to level up, which would tell the function how many dice to roll. Then it would display that number of dice on the popup
 $(document).ready(() => {
     //everything jQuery goes in here
     let $levelButton = $("#levelTrigger");
+    let $popup = $(".hoverBkgr");
+    let $byeButton = $(".popupClose");
 
     $levelButton.on({
         click: () => {
-            //im thinking the dice roll function'll prob go here and also maybe show a display of previous health max to current health max?
+            $popup.show();
         }
-    })
-    $(window).load(function () {
-        $("#levelTrigger").click(function () {
-            $('.hoverBkgr').show();
-        });
-        $('.hoverBkgr').click(function () {
-            $('.hoverBkgr').hide();
-        });
-        $('.popupClose').click(function () {
-            $('.hoverBkgr').hide();
-        });
+    });
+    $byeButton.on({
+        click: () => {
+            $byeButton.hide();
+        }
     });
 
 });
-
+//we somehow need to make the dice show up in the popup
 let levelButton = document.getElementById("levelTrigger");
 levelButton.addEventListener("click", () => {
     rollDice();
@@ -53,4 +49,30 @@ function rollDice() {
         dice.src = "https://upload.wikimedia.org/wikipedia/commons/f/f4/Alea_6.png";
         find.appendChild(dice);
     }
+}
+
+//here iis my code for the dice roller. Im putting it here cause idk if youd want to use it since its less code
+let rollbutton = document.getElementById("button");
+button.addEventListener("click", () => {
+    rollDice(document.getElementById("textbox").value)
+})
+
+function rollDice(number) {
+
+    var dice = document.getElementById("dice");
+
+    for (let i = dice.children.length - 1; i >= 0; i--) {
+        var die = dice.children[i];
+        die.remove();
+    }
+
+    var imgs = ["https://upload.wikimedia.org/wikipedia/commons/2/2c/Alea_1.png", "https://upload.wikimedia.org/wikipedia/commons/b/b8/Alea_2.png", "https://upload.wikimedia.org/wikipedia/commons/2/2f/Alea_3.png", "https://upload.wikimedia.org/wikipedia/commons/8/8d/Alea_4.png", "https://upload.wikimedia.org/wikipedia/commons/5/55/Alea_5.png", "https://upload.wikimedia.org/wikipedia/commons/f/f4/Alea_6.png"];
+
+    for (let i = 0; i < number; i++) {
+        var img1 = document.createElement("img");
+        var x = Math.floor(Math.random() * 6);
+        img1.setAttribute("src", imgs[x]);
+        dice.appendChild(img1);
+    }
+
 }
